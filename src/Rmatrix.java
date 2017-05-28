0edd64c465daee784ee1b9da7331337d7116e0e7
Keet Sugathadasa <Keet Sugathadasa>
2017-05-28 11:21:25 +0530

    Modified the RMatrix Class

--- src/Rmatrix.java
+++ src/Rmatrix.java
@@ -22,8 +22,9 @@ public class Rmatrix {
         }
 
         for (int i =0 ; i <Constants.C_CONSTANT * Constants.L_GT_WORD_COUNT ; i++ ){
-            rMatrix[1][i] = "0.0";
+            rMatrix[1][i] = word2vecModel.getOutputDistanceList(index)[i];
         }
+
 
 
     }
