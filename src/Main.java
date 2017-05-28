fbf5db0fd30d16369b471948ef7a31417b6211e5
Keet Sugathadasa <Keet Sugathadasa>
2017-05-28 13:29:04 +0530

    added a code to serialize and deserialize VactorMatrices array

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@
  * Project - SemanticSimilarityMeasure
  */
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.Objects;
 
 public class Main {
@@ -15,36 +13,49 @@ public class Main {
         //reading from CSV file
         CSV csvObject = new CSV();
 
-//        ArrayList<ValueMatrix> valueMatrices = new ArrayList<ValueMatrix>();
-//        ArrayList<Rmatrix> miMatrices = new ArrayList<Rmatrix>();
-
         ValueMatrix[] valueMatrices = new ValueMatrix[100];
 
         //call word2vec model
         Word2vecModel word2vecModel = new Word2vecModel();
 
-        int i=0;
-        for (String term : csvObject.getTermList()) {
-
-            Matrix matrixObject = new Matrix(term, word2vecModel, csvObject);
-            Normalize normalize = new Normalize(matrixObject.getMatrix());
-            ValueMatrix valueMatrix = new ValueMatrix(term, csvObject, word2vecModel, normalize);
-            Rmatrix rmatrix = new Rmatrix(term, csvObject, word2vecModel);
-            valueMatrix.setRmatrix(rmatrix);
-
-            valueMatrices[i] = valueMatrix;
-            System.out.println("Value Matrix for term = " + term + " , done");
-
-            i++;
-
-
-            //delete this. This is to avoid the for loop from calculating all 100 terms. This will skip after the first term "layer"
-            if (Objects.equals(term, "lawyer")) break;
-
-        }
-
-        //Serialize the ValueMatrix Array to src
         try {
+            File file = new File("src/valueMatrices.ser");
+            if (file.exists()) {
+
+                FileInputStream fileIn = new FileInputStream(file);
+                ObjectInputStream in = new ObjectInputStream(fileIn);
+                valueMatrices = (ValueMatrix[]) in.readObject();
+                in.close();
+                fileIn.close();
+            } else {
+
+                valueMatrices = new ValueMatrix[100];
+                int i = 0;
+                for (String term : csvObject.getTermList()) {
+
+                    Matrix matrixObject = new Matrix(term, word2vecModel, csvObject);
+                    Normalize normalize = new Normalize(matrixObject.getMatrix());
+                    ValueMatrix valueMatrix = new ValueMatrix(term, csvObject, word2vecModel, normalize);
+                    Rmatrix rmatrix = new Rmatrix(term, csvObject, word2vecModel);
+                    valueMatrix.setRmatrix(rmatrix);
+
+                    valueMatrices[i] = valueMatrix;
+                    System.out.println("Value Matrix for term = " + term + " , done");
+
+                    i++;
+
+
+                    //delete this. This is to avoid the for loop from calculating all 100 terms. This will skip after the first term "layer"
+                    //if (Objects.equals(term, "lawyer")) break;
+
+                }
+
+            }
+
+
+
+
+            //Serialize the ValueMatrix Array to src
             FileOutputStream fileOut =
                     new FileOutputStream("src/valueMatrices.ser");
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
@@ -52,8 +63,12 @@ public class Main {
             out.close();
             fileOut.close();
             System.out.printf("Serialized data is saved in src/valueMatrices.ser");
-        }catch(IOException exception) {
+        } catch (IOException exception) {
             exception.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+            return;
         }
 
 
@@ -62,33 +77,12 @@ public class Main {
         int maxRuns = 50000;
         double minErrorCondition = 0.001;
         neuralNetwork.setInputVector(valueMatrices);
-        neuralNetwork.run(maxRuns, minErrorCondition,csvObject);
+        neuralNetwork.run(maxRuns, minErrorCondition, csvObject);
 
-
-
-        //        //call neural network
-//        NeuralNetwork nn = new NeuralNetwork(2, 4, 1);
-//        int maxRuns = 50000;
-//        double minErrorCondition = 0.001;
-//        nn.run(maxRuns, minErrorCondition);
-
-        //run( "judge","lawyer" );
 
     }
 
-//    private static ILexicalDatabase db = new NictWordNet();
-//    private static RelatednessCalculator[] rcs = {
-//            new HirstStOnge(db), new LeacockChodorow(db), new Lesk(db), new WuPalmer(db),
-//            new Resnik(db), new JiangConrath(db), new Lin(db), new Path(db)
-//    };
-//
-//    private static void run(String word1, String word2) {
-//        WS4JConfiguration.getInstance().setMFS(true);
-//        for (RelatednessCalculator rc : rcs) {
-//            double s = rc.calcRelatednessOfWords(word1, word2);
-//            System.out.println(rc.getClass().getName() + "\t" + s);
-//        }
-//    }
+
 
 
 }
