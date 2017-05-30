a422f6f6c5c83586af99234ebb801c7bd450e0c9
Nisansa Dilushan de Silva <NisansaDdS@cse.mrt.ac.lk>
2017-05-30 04:59:02 -0700

    Another P change

--- src/Word2VecEvaluator.java
+++ src/Word2VecEvaluator.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Created by Nisansa on 5/29/2017.
  */
@@ -10,6 +13,7 @@ public class Word2VecEvaluator {
 
     private void run(){
         String[] fileNames = {"Word2VecResult[G].csv" , "Word2VecResult[LR].csv", "Word2VecResult[LL].csv"};
+        StringBuilder sb=new StringBuilder();
         for (int i = 0 ; i <3 ; i++) {
             //Loop for G, LR and LL files one by one {  //Keet must update this DONE
             String filename = "CSV/Word2VecEvaluator/"+fileNames[i]; //G, LR and LL files //Keet must update this DONE
@@ -21,7 +25,6 @@ public class Word2VecEvaluator {
             double recall = 0;
 
             int[] K = {20, 50, 100, 200, 500};
-
             for (int k : K) {
 
                 for (int p = 0; p < numOfInptLines; p++) {
@@ -40,14 +43,30 @@ public class Word2VecEvaluator {
 
                 System.out.println("k = " + k);
                // System.out.printf("Error = %.2f\n", error);
-                System.out.printf("Precision = %.2f\n", (1-error));
+                double f1=(2*(1-error) * recall / ((1-error) + recall));
+
+                error=new BigDecimal(error).setScale(2, RoundingMode.HALF_UP).doubleValue();
+                recall=new BigDecimal(recall).setScale(2, RoundingMode.HALF_UP).doubleValue();
+                f1=new BigDecimal(f1).setScale(2, RoundingMode.HALF_UP).doubleValue();
+                double p=1-error;
+                sb.append(p);
+                sb.append(" & ");
+                sb.append(recall);
+                sb.append(" & ");
+                sb.append(f1);
+                sb.append(" & ");
+
+
+                System.out.printf("Precision = %.2f\n", p);
+
                 System.out.printf("Recall = %.2f\n", recall);
-                System.out.printf("F1 = %.2f\n", (2*(1-error) * recall / ((1-error) + recall)));
+                System.out.printf("F1 = %.2f\n", f1);
                 System.out.println("..............");
             }
-
+            sb.append("\n");
             System.out.println("----------------Next File-----------------");
         }
+        System.out.println(sb.toString());
         //}  //uncomment //Keet must update this
     }
 
